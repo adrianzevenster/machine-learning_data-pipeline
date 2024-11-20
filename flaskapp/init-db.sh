c157c4a5c9b2081445c7b6b73fb57279699c6ae8
adrianzevenster <64969874+adrianzevenster@users.noreply.github.com>
2024-11-20 20:14:01 +0200

    mysqldump

--- flaskapp/init-db.sh
+++
@@ -1,8 +0,0 @@
-
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" RawData -e "
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/RawData.csv.gz'
-INTO TABLE DP_CDR_Data
-FIELDS TERMINATED BY ','
-ENCLOSED BY '\"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;"
